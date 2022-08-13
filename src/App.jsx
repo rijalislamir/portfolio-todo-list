103dab95e87eb74d0f2260650298e59a66b2468f
Rijal Islami <rijalislamir@gmail.com>
2022-08-13 10:50:51 +0700

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 import './App.css'
 
 function App() {
 	return (
 		<div>
-			Hello World
+			<Navbar/>
 		</div>
 	)
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <span>TO DO LIST APP</span>
+        </nav>
+    )
+}
+
+export default Navbar
